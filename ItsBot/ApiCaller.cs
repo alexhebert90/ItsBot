20e80dca97441bbe7c535a84d04e21cc4329840e
Alex Hebert <alexhebert90@gmail.com>
2017-09-23 20:41:42 -0500

    First Commit

---
+++ ItsBot/ApiCaller.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ItsBot
+{
+    internal class ApiCaller
+    {
+        /// <summary>
+        /// This should be the root url used for all api endpoints.
+        /// </summary>
+        private const string API_URL = "https://www.reddit.com/api/v1/";
+
+        private HttpClient Client { get; }
+
+        public ApiCaller(string userAgent)
+        {
+            // Since the use case for this is pretty specific, I'm going to require a
+            // user agent to be provided and set.
+            if(userAgent == null)
+                throw new ArgumentNullException(nameof(userAgent));
+
+            Client = new HttpClient();
+
+            // Remove any default headers.
+            Client.DefaultRequestHeaders.Clear();
+            Client.DefaultRequestHeaders.Add("User-Agent", userAgent);
+
+        }
+
+         
+
+        public async Task<string> CallAsync(string requestUri)
+            => await CallAsync(requestUri, null);
+
+        public async Task<string> CallAsync(string requestUri, FormUrlEncodedContent formContent)
+        {
+            var result = await Client.PostAsync(requestUri, formContent);
+
+            return result.Content.ToString();
+
+        }
+    }
+}
---
+++ ItsBot/TokenManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+
+namespace ItsBot
+{
+    internal class TokenManager
+    {
+        private const string ENDPOINT = "access_token";
+
+        private readonly KeyValuePair<string, string> _grantType =
+            new KeyValuePair<string, string>("grant_type", "installed_client");
+
+        private const string USER_AGENT = "ItsBot-TokenAgent";
+
+        // ToDo: DeviceId
+
+        private ApiCredentials Credentials { get; }
+
+        private ApiCaller Api { get; }
+
+        public TokenManager(ApiCredentials apiCredentials)
+        {
+            Credentials = apiCredentials ?? throw new ArgumentNullException(nameof(apiCredentials));
+            Api = new ApiCaller(USER_AGENT);
+        }
+
+        public string Token => GetToken();
+
+        private string GetToken()
+        {
+            // ToDo: Manage not getting a new key with every request.
+            return NewToken();
+        }
+
+
+        private string NewToken()
+        {
+            var formData = new FormUrlEncodedContent(new[]
+            {
+                _grantType
+            });
+
+            var apiResponse = Api.CallAsync(ENDPOINT, formData).Result;
+
+            return apiResponse;
+        }
+    }
+}
